e35a88b2cd277287930c4e0faadefa911b690dc1
Roman <romagritcaenko@gmail.com>
2025-07-03 16:57:37 +0300

    ast

---
+++ src/ast.zig
@@ -0,0 +1,31 @@
+const Expression = union(enum) {
+    const InfixChildren = struct { left: *Expression, right: *Expression };
+    const IfChildren = struct { condition: *Expression, then: *Expression, alternative: ?*Expression };
+
+    IntLiteral: i32,
+    Boolean: bool,
+    Identifier: []const u8,
+
+    // prefix operator
+    Not: *Expression, // index of another expression
+    Negative: *Expression, // index of another expression
+
+    // infix operator
+    Equals: InfixChildren,
+    NotEquals: InfixChildren,
+    Add: InfixChildren,
+    Subtract: InfixChildren,
+    Multiply: InfixChildren,
+    Divide: InfixChildren,
+    GreaterThan: InfixChildren,
+    LessThan: InfixChildren,
+
+    If: IfChildren,
+    Func: struct {
+        args: [][]const u8,
+        statements: []const Statement,
+    },
+    Call: struct { func: *Expression, args: []const Expression },
+};
+
+const Statement = union(enum) { Expression: Expression, Return: Expression, Let: struct { name: []const u8, expr: *Expression } };
